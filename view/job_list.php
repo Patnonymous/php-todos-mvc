3e927f9c994098532641988eb6e666891f571798
Patrick_Williamson <patrickw9080@outlook.com>
2022-04-18 15:52:53 -0400

    Updated job list view.

--- view/job_list.php
+++ view/job_list.php
@@ -21,6 +21,63 @@
             <button class="add-button bold">View</button>
         </form>
     </header>
+
+    <?php if ($jobs) { ?>
+        <?php foreach ($jobs as $job) : ?>
+            <div class="list__row">
+                <div class="list__item">
+                    <p class="bold"><?= $job['categoryName'] ?></p>
+                    <p><?= $job['Description'] ?></p>
+                </div>
+                <div class="list__remove_item">
+                    <form action="." method="post">
+                        <input type="hidden" name="action" value="delete_job">
+                        <input type="hidden" name="job_id" value="<?= $job['JobID'] ?>">
+                        <button class="remove-button">Del</button>
+                    </form>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    <?php } else { ?>
+        <br>
+
+        <?php if ($categoryId) { ?>
+            <p>No Jobs in this category.</p>
+        <?php } else { ?>
+            <p>No Jobs.</p>
+        <?php } ?>
+
+        <br>
+    <?php } ?>
 </section>
+
+
+<section id="add" class="add">
+    <h2>Add Job</h2>
+    <form action="." method="post" id="add__form" class="add__form">
+        <input type="hidden" name="action" value="add_job">
+        <div class="add__inputs">
+            <label>Category: </label>
+            <select name="category_id" required>
+                <option value="">Select Category</option>
+                <?php foreach ($categories as $category) : ?>
+                    <option value="<?= $category['CategoryID']; ?>">
+                        <?= $category['CategoryName']; ?>
+                    </option>
+                <?php endforeach; ?>
+            </select>
+
+            <label>Description:</label>
+            <input type="text" name="description" maxlength="120" placeholder="Description/Job Name" required>
+        </div>
+
+        <div class="add__addItem">
+            <button class="add-button bold">Add</button>
+        </div>
+    </form>
+</section>
+
+<br>
+<p><a href=".?action=list_categories">View or Edit Categories</a></p>
 
 <?php include('view/footer.php'); ?>
