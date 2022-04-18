b0d587a38409a441071e2803817f970a92695ada
Patrick_Williamson <patrickw9080@outlook.com>
2022-04-18 15:01:48 -0400

    Fixed multiple db related typos.

--- model/db_jobs.php
+++ model/db_jobs.php
@@ -4,22 +4,26 @@ function getJobsByCategory($categoryId)
 {
     global $db;
     if ($categoryId) {
-        $query = 'SELECT j.JobID, j.Description, c.CategoryName FROM job a 
+        $query = 'SELECT j.JobID, j.Description, c.CategoryName FROM job j 
             LEFT JOIN category c ON j.FKCategoryID = c.CategoryID 
             WHERE j.JobID = :categoryId 
             ORDER BY j.JobID';
+        $stmt = $db->prepare($query);
+        $stmt->bindValue(':categoryId', $categoryId);
+        $stmt->execute();
+        $jobs = $stmt->fetchAll();
+        $stmt->closeCursor();
+        return $jobs;
     } else {
-        $query = 'SELECT j.JobID, j.Description, c.CategoryName FROM job a 
+        $query = 'SELECT j.JobID, j.Description, c.CategoryName FROM job j 
             LEFT JOIN category c ON j.FKCategoryID = c.CategoryID 
             ORDER BY c.categoryID';
+        $stmt = $db->prepare($query);
+        $stmt->execute();
+        $jobs = $stmt->fetchAll();
+        $stmt->closeCursor();
+        return $jobs;
     }
-
-    $stmt = $db->prepare($query);
-    $stmt->bindValue(':categoryId', $categoryId);
-    $stmt->execute();
-    $jobs = $stmt->fetchAll();
-    $stmt->closeCursor();
-    return $jobs;
 }
 
 
